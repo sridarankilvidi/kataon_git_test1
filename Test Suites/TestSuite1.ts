cc004a030048edf8657d7094cf4a03ee276bc29c
sridarankilvidi <sridaran.kilvidi@gmail.com>
2019-01-05 17:34:36 -0500

    this is test2

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9a46ec51-592f-43ba-85d1-15e7aebcf8a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>453e54a3-457a-4bfb-bbf3-a6ad0921ad0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase1-from sridaran</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
